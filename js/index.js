88363e0ab4e6aebbd89f6043a397e29923c43385
sreekar2307 <bollamsreekhar@gmail.com>
2018-09-21 17:54:10 +0530

    Path config

--- js/index.js
+++ js/index.js
@@ -4,9 +4,9 @@ function Reviews(image_url,person_name,review_statement){
 	this.review_statement = review_statement;
 };
 
-var Review1 = new Reviews("img/person.jpeg","Eiusmod laboris","“Incididunt nulla cillum adipisicing aliqua aliquip et veniam elit elit fugiat id ullamco anim aliquip sint quis sed esse ad cupidatat eiusmod laborum voluptate.”"); 
-var Review2 = new Reviews("img/person_1.jpeg","Lorem ipsum","“Exercitation enim anim sunt ad non est ut eu aliquip cupidatat ut labore esse irure eiusmod aliquip duis dolore culpa est ut aliqua aute aliqua eiusmod aute aliqua.”");
-var Review3 = new Reviews("img/person_2.jpeg","Enim in","“Anim consequat et dolor exercitation mollit eiusmod do est occaecat proident aute esse proident occaecat aliquip adipisicing fugiat quis deserunt amet est ad dolor veniam ut non adipisicing.”"); 
+var Review1 = new Reviews("../img/person.jpeg","Eiusmod laboris","“Incididunt nulla cillum adipisicing aliqua aliquip et veniam elit elit fugiat id ullamco anim aliquip sint quis sed esse ad cupidatat eiusmod laborum voluptate.”"); 
+var Review2 = new Reviews("../img/person_1.jpeg","Lorem ipsum","“Exercitation enim anim sunt ad non est ut eu aliquip cupidatat ut labore esse irure eiusmod aliquip duis dolore culpa est ut aliqua aute aliqua eiusmod aute aliqua.”");
+var Review3 = new Reviews("../img/person_2.jpeg","Enim in","“Anim consequat et dolor exercitation mollit eiusmod do est occaecat proident aute esse proident occaecat aliquip adipisicing fugiat quis deserunt amet est ad dolor veniam ut non adipisicing.”"); 
 
 var reviews=[Review1, Review2 , Review3];
 
