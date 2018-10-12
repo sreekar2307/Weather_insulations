c46bea10d40cefcbafd9707821f5bef476778db2
Sreekar2307 <bollamsreekhar@gmail.com>
2018-10-12 22:35:18 +0530

    added signup page

---
+++ js/signup.js
@@ -0,0 +1,41 @@
+var icon =document.querySelector(".password i");
+var input = document.querySelector(".password input");
+var flag = true;
+
+icon.addEventListener("click",function(){
+if(flag)
+{
+	icon.classList.add("fa-eye");
+	icon.classList.remove("fa-eye-slash");
+	input.type="text";
+}
+else{
+	input.type="password";
+	icon.classList.add("fa-eye-slash");
+	icon.classList.remove("fa-eye");	
+}
+flag=!flag;
+});
+
+input.addEventListener("keyup",function(){
+	if(input.validity.tooShort)
+		 document.querySelector(".error p ").innerText="minimum length is 6 characters";
+	else if(input.validity.patternMismatch)
+	     document.querySelector(".error p ").innerText="should contain atleast one numeric, special characters like '@#$_' are only allowed";
+	else
+	    document.querySelector(".error p ").innerText="";    	
+})
+
+var tel = document.querySelector(".phone_number input");
+
+tel.addEventListener("keyup",function(){
+  if(tel.checkValidity()){
+   document.querySelector(".phone_number i").classList.add("fa-check-circle");
+   document.querySelector(".phone_number i").classList.remove("fa-times-circle");
+  }
+  else{
+    document.querySelector(".phone_number i").classList.remove("fa-check-circle");
+    document.querySelector(".phone_number i").classList.add("fa-times-circle");
+  }
+});
+
